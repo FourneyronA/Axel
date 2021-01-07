83bdb7cc2760de97baaa0bfb1f9856f01fd90c8b
Axel Fourneyron <fourneyron.axel.lucas@gmail.com>
2021-01-07 02:08:47 +0100

    add news map video

--- assets/js/gallery_fonction.js
+++ assets/js/gallery_fonction.js
@@ -1,5 +1,9 @@
 function myFunction(imgs) {
   // Get the expanded image
+  var y = document.getElementById("video_carto_punk");
+	if(y.style.display == "block"){
+		y.style.display = "none";
+	} 
   var expandImg = document.getElementById("expandedImg");
 
   // Use the same src in the expanded image as the image being clicked on from the grid
@@ -8,3 +12,27 @@ function myFunction(imgs) {
   // Show the container element (hidden with CSS)
   expandImg.parentElement.style.display = "block";
 } 
+
+
+var video_carto_punk = document.getElementById("video_carto_punk");
+video_carto_punk.style.display = "none";
+
+document.getElementById("carto_punk").addEventListener("click", function () {
+  
+  var z = document.getElementById("container");
+	if(z.style.display == "block"){
+		z.style.display = "none";
+	} 
+  
+  var y = document.getElementById("video_carto_punk");
+	if(y.style.display == "block"){
+		y.style.display = "none";
+	} else{
+		y.style.display = "block";
+  }
+  
+  
+	 
+});
+
+
